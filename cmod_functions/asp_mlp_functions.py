4a01ab6d7860f6dc949653a4819f634dd5edac33
sajidah-ahmed <sah033@uit.no>
2022-03-29 14:07:57 -0400

    Upload functions relevant for scanning and dwell MLP data

---
+++ cmod_functions/asp_mlp_functions.py
@@ -0,0 +1,119 @@
+import MDSplus as mds
+
+# Node names for ASP MLP data, used by get_signal_asp_mlp
+variables_dictionary_asp_mlp = {
+    "ne": "DENSITY_FIT",
+    "Is": "ISAT_FIT",
+    "Js": "JSAT_FIT",
+    "Vp": "PHI_FIT",
+    "Te": "TE_FIT",
+    "Vf": "VF_FIT",
+}
+
+
+def get_plunge_depth(shot_number: int):
+    """
+    Extracts the plunge distance and corresponding time of the probe.
+
+    Args:
+        shot_number: Shot number(s) of interest.
+
+    Returns:
+        plunge_time: Corresponding time data in seconds.
+        plunge: Plunge depth of probe in metres and assumed purely horizontal.
+    """
+
+    c = mds.Connection("alcdata")
+    c.openTree("edge", shot_number)
+
+    dataname_plunge = "\edge::top.probes.asp.plunge"
+
+    plunge = c.get(dataname_plunge).data()
+    plunge_time = c.get("dim_of(" + dataname_plunge + ")").data()
+
+    return plunge_time, plunge
+
+
+def get_probe_origin(shot_number: int):
+    """
+    Extracts the probe origin.
+
+    Args:
+        shot_number: Shot number(s) of interest.
+
+    Returns:
+        origin: Probe origin giving (R, Z, R*Phi) of the probe, *EXPLAIN R, Z, Phi*
+    """
+
+    dataname_origin = "\edge::top.probes.asp.g_1.origin"
+    c = mds.Connection("alcdata")
+    c.openTree("edge", shot_number)
+
+    origin = c.get(dataname_origin).data()
+
+    return origin
+
+
+def get_asp_mlp_rho(shot_number, probe_pin_number):
+    """
+    Extracts the rho, the distance relative to the last-closed flux surface, of a probe tip.
+
+    Args:
+        shot_number: Shot number(s) of interest.
+        probe_pin_number: Particluar probe tip usually from 0 to 3.
+
+    Returns:
+        rho_time: Time data for rho
+        rho: The probe position relative to the separatrix
+    """
+
+    c = mds.Connection("alcdata")
+    c.openTree("edge", shot_number)
+
+    dataname_rho = f"\edge::top.probes.asp.mlp.p{probe_pin_number}:rho"
+
+    rho = c.get(dataname_rho)
+    rho_time = c.get("dim_of(" + dataname_rho + ")").data()
+
+    return rho_time, rho
+
+
+def get_asp_mlp_data(
+    shot_number, probe_pin_number, variable_name, time_start=None, time_end=None
+):
+    """
+    Extracts raw mirror-Langmuir probe (MLP) data.
+
+    Args:
+        shot_number: Shot number(s) of interest.
+        probe_pin_number: Particluar probe tip usually from 0 to 3.
+        variable_name: The variable of interests
+            variables_dictionary_asp_mlp = {
+            "ne": "DENSITY_FIT",
+            "Is": "ISAT_FIT",
+            "Js": "JSAT_FIT",
+            "Vp": "PHI_FIT",
+            "Te": "TE_FIT",
+            "Vf": "VF_FIT"}
+        time_start: Start time of interest.
+        time_end: End time of interest.
+
+    Returns:
+        asp_mlp_time: Time data for MLP.
+        asp_mlp_data: Raw MLP data of a particular variable.
+    """
+
+    c = mds.Connection("alcdata")
+    c.openTree("edge", shot_number)
+
+    dataname = f"\edge::top.probes.asp.mlp.p{probe_pin_number}:{variables_dictionary_asp_mlp[variable_name]}"
+
+    asp_mlp_data = c.get(dataname).data()
+
+    asp_mlp_time = c.get("dim_of(" + dataname + ")").data()
+
+    if (time_start is not None) & (time_end is not None):
+        time_interval = (asp_mlp_time > time_start) & (asp_mlp_time < time_end)
+        return asp_mlp_time[time_interval], asp_mlp_data[time_interval]
+
+    return asp_mlp_time, asp_mlp_data
