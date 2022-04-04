4ec63e8b828d92ed9a871b907e2dcb128e50c384
sajidah-ahmed <sah033@uit.no>
2022-04-04 14:01:42 -0400

    Change function names

--- cmod_functions/asp_mlp_functions.py
+++ cmod_functions/asp_mlp_functions.py
@@ -1,5 +1,7 @@
 import MDSplus as mds
 
+from cmod_functions.apd_functions import generate_raw_apd_dataset
+
 """
 Abbreviations:
     MLP: mirror-Langmuir probe
@@ -84,7 +86,7 @@ def get_asp_mlp_rho(shot_number, probe_pin_number):
     return rho_time, rho
 
 
-def get_asp_mlp_data(
+def get_raw_asp_mlp_data(
     shot_number, probe_pin_number, variable_name, time_start=None, time_end=None
 ):
     """
@@ -123,3 +125,77 @@ def get_asp_mlp_data(
         return asp_mlp_time[time_interval], asp_mlp_data[time_interval]
 
     return asp_mlp_time, asp_mlp_data
+
+
+def generate_raw_scanning_mlp_data(shot_number, variable_name):
+    """
+    Extracts raw scanning mirror-Langmuir probe (MLP) data.
+
+    Args:
+        shot_number: Shot number(s) of interest.
+        probe_pin_number: Particluar probe tip usually from 0 to 3.
+        variable_name: The variable of interests
+            variables_dictionary_asp_mlp = {
+            "ne": "DENSITY_FIT",
+            "Is": "ISAT_FIT",
+            "Js": "JSAT_FIT",
+            "Vp": "PHI_FIT",
+            "Te": "TE_FIT",
+            "Vf": "VF_FIT"}
+
+    Returns:
+        time_common: Common time data of all four probe bins collecting data.
+        time_series_average: Average raw MLP data of a particular variable from all four pin.
+        rho_time: Time data of rho.
+        probe_raw_time: Time data of rho interpolated onto MLP time base.
+        rho_average: Average rho data of all four probe pins.
+        plunge-radius: Major radius position of probe relative to the probe origin.
+    """
+
+    import numpy as np
+    from scipy.interpolate import interp1d
+    from functools import reduce
+
+    plunge_time, plunge = get_plunge_depth(shot_number)
+    origin = get_probe_origin(shot_number)
+
+    plunge_radius = plunge - origin[0]
+
+    time_series_list = []
+    time_list = []
+    rho_list = []
+    rho_time_list = []
+
+    for probe_pin_number in [0, 1, 2, 3]:
+        asp_mlp_time, asp_mlp_data = get_raw_asp_mlp_data(
+            variable_name, probe_pin_number, shot_number)
+
+        rho_time, rho = get_asp_mlp_rho(shot_number, probe_pin_number)
+
+        time_series_list.append(asp_mlp_data)
+        time_list.append(asp_mlp_time)
+        rho_list.append(rho)
+        rho_time_list.append(rho_time)
+
+    time_common = reduce(np.intersect1d, time_list)
+    rho_time = rho_time_list[0]
+
+    rho_average = 0.25 * (
+        rho_list[0]
+        + rho_list[1]
+        + rho_list[2]
+        + rho_list[3]
+    )
+
+    # Interpolate plunge onto the MLP timebase
+    probe_rho_interpolate = interp1d(rho_time, rho_average)
+    probe_rho_time = probe_rho_interpolate(time_common)
+
+    time_series_average = 0.25 * (
+        time_series_list[0]
+        + time_series_list[1]
+        + time_series_list[2]
+        + time_series_list[3]
+    )
+
+    return time_common, time_series_average, rho_time, probe_rho_time, rho_average, plunge_radius
