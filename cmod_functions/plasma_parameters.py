2a68b11327eddb739c9527fb3ff48ce9b6b27965
sajidah-ahmed <sajidah.ahmed@bath.edu>
2022-04-13 14:07:28 -0400

    Use f-strings

--- cmod_functions/plasma_parameters.py
+++ cmod_functions/plasma_parameters.py
@@ -18,12 +18,11 @@ def get_line_integrated_density(shot_number):
     c = mds.Connection("alcdata")
     c.openTree("electrons", shot_number)
 
-    line_integrated_density_dataname = f"\ELECTRONS::TOP.TCI.RESULTS.NL_04"
+    line_integrated_density_dataname = "\ELECTRONS::TOP.TCI.RESULTS.NL_04"
 
     line_integrated_density = c.get(line_integrated_density_dataname).data()
-    line_integrated_density_time = c.get(
-        "dim_of(" + line_integrated_density_dataname + ")"
-    ).data()
+    line_integrated_density_time = c.get(f"dim_of({line_integrated_density_dataname})").data()
+
 
     return line_integrated_density_time, line_integrated_density
 
@@ -45,12 +44,11 @@ def get_line_averaged_density(shot_number):
     c = mds.Connection("alcdata")
     c.openTree("electrons", shot_number)
 
-    line_averaged_density_dataname = f"\ELECTRONS::TOP.TCI.RESULTS.INVERSION.NEBAR_EFIT"
+    line_averaged_density_dataname = "\ELECTRONS::TOP.TCI.RESULTS.INVERSION.NEBAR_EFIT"
 
     line_averaged_density = c.get(line_averaged_density_dataname).data()
-    line_averaged_density_time = c.get(
-        "dim_of(" + line_averaged_density_dataname + ")"
-    ).data()
+    line_averaged_density_time = c.get(f"dim_of({line_averaged_density_dataname})").data()
+
 
     return line_averaged_density_time, line_averaged_density
 
@@ -72,9 +70,9 @@ def get_plasma_current(shot_number):
     c = mds.Connection("alcdata")
     c.openTree("magnetics", shot_number)
 
-    plasma_current_dataname = f"\MAGNETICS::IP/1000"
+    plasma_current_dataname = "\MAGNETICS::IP/1000"
     plasma_current = c.get(plasma_current_dataname).data()
-    plasma_current_time = c.get("dim_of(" + plasma_current_dataname + ")").data()
+    plasma_current_time = c.get(f"dim_of({plasma_current_dataname})").data()
 
     return plasma_current_time, plasma_current
 
@@ -94,11 +92,10 @@ def get_toroidal_magnetic_field(shot_number):
     c = mds.Connection("alcdata")
     c.openTree("magnetics", shot_number)
 
-    toroidal_magnetic_field_dataname = f"\MAGNETICS::BTOR"
+    toroidal_magnetic_field_dataname = "\MAGNETICS::BTOR"
 
     toroidal_magnetic_field = -c.get(toroidal_magnetic_field_dataname).data()
-    toroidal_magnetic_field_time = c.get(
-        "dim_of(" + toroidal_magnetic_field_dataname + ")"
-    ).data()
+    toroidal_magnetic_field_time = c.get(f"dim_of({toroidal_magnetic_field_dataname})").data()
+
 
     return toroidal_magnetic_field_time, toroidal_magnetic_field
