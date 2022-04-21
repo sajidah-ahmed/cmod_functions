9d7439d8014139d276a02640e27d24c0abac42b6
gregordecristoforo <gregor.decristoforo@gmail.com>
2022-04-21 13:40:32 -0400

    exception message added in phantom functions

--- cmod_functions/phantom_functions.py
+++ cmod_functions/phantom_functions.py
@@ -99,7 +99,9 @@ def generate_phantom_dataset(
             },
         )
 
-    except:
+    except Exception:
+        print(f"No R and Z data available for shot {shot_number}")
+
         return xr.Dataset(
             {"frames": (["time", "y", "x"], frames)},
             coords={"time": (["time"], time)},
