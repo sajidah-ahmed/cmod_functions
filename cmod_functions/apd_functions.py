c16e6d6444d6080078f398b18de32110d1a9e56a
sajidah-ahmed <sajidah.ahmed@bath.edu>
2022-05-05 15:10:26 -0400

    Function to change from R-Z to rho

--- cmod_functions/apd_functions.py
+++ cmod_functions/apd_functions.py
@@ -187,3 +187,125 @@ def _create_xr_dataset(apd_signal_array, time, time_start, time_end, R, Z):
         {"frames": (["y", "x", "time"], frames)},
         coords={"R": (["y", "x"], R), "Z": (["y", "x"], Z), "time": (["time"], time)},
     )
+
+
+def efit_major_radius_to_rho(R, Z, time_array, shot_number, tree):
+    """
+    Converts radial and poloidal coordinates to flux surface coordinates, rho.
+
+    Args:
+        R: Major radius (in cm) array for the coordinate pair to be mapped to rho.
+        Z: Height (above the machine midplane) (in cm) array for the coordinate pair to be mapped to rho.
+        time_array: Time array in seconds for the times for which the mapping is desired.
+        shot_number: Shot number of interest.
+        tree: Set this equal to the string of the tree name (e.g. 'efit19') which will be used
+                for the flux surface mapping. The default is 'analysis'
+
+    Returns:
+        rho: 2D array (with the 1st dimension having the same number of
+            elements as inputs R and Z and the 2nd dimension having the
+            same number of elements as the input time_array).
+            The 1st dimension specifies the element of the coordinate pair (R, Z) for the
+            flux surface mapping from the coordinate (R, Z) at time time_array(i)
+            to the distance beyond the separatrix in the horizontal plane
+            that is defined by the Z of the magnetic axis (ZMAGX) at
+            time time_array(i). Rho is returned in cm, with positive sign being outside
+            the separatrix and minus sign being inside the separatrix.
+
+    """
+
+    import eqtools as eq
+    import numpy as np
+
+    cmod_efit = eq.CModEFITTree(shot_number, tree)
+
+    # Major radii (in m) of the R,Z coordinate arrays
+    # flux-surface-mapped to the height of the magnetic axis at each
+    # time of the time_array. NOTE: that the time index is the 1st one, the space index is the 2nd!
+    rmid = cmod_efit.rz2rmid(R / 100.0, Z / 100.0, time_array)
+
+    # rmidout is the major radius of the outboard side of the separatrix at the
+    # height of the magnetic axis (m)
+    rmidout = cmod_efit.getRmidOut()
+    efit_time = cmod_efit.getTimeBase()
+
+    rho = np.zeros((len(R), len(time_array)))
+
+    for i in range(len(time_array)):
+
+        # Rhos is now the array of distances between the R,Z coordinate array
+        # points that have been flux-surface-mapped to the height of the
+        # magnetic axis MINUS the major radius of the outboard side of the
+        # separatrix at the height of the magnetic axis (in cm)
+
+        conditional = np.where(
+            np.abs(time_array[i] - efit_time)
+            == np.amin(np.abs(time_array[i] - efit_time))
+        )
+        rho[:, i] = (rmid[i, :] - rmidout[conditional]) * 100.0
+
+    return rho
+
+
+def major_radius_to_average_rho(
+    shot_number, time_start=None, time_end=None, resolution=resolution, tree="analysis"
+):
+    """
+    Given the pixel locations and the time slice, this function converts radial and poloidal coordinates to flux surface coordinates, rho.
+
+    Args:
+        shot_number: Shot number of interest.
+        time_start: The beginning of the time window in seconds. Set to None by default.
+        time_end: The end of the time window in seconds. Set to None by default.
+        resolution: The number of time points. Usually set to 300 time points which is the maximum for efit19.
+        tree: Set this equal to the string of the tree name (e.g. 'efit19') which will be used
+                for the flux surface mapping. The default is 'analysis'
+
+    Returns:
+        R: Array of radial coordinates in centimetres.
+        Z: Array of the height in centimetres.
+        rho_time_averaged: The time-averaged flux mapped coordinate in centimetres based on the time window specified.
+
+    """
+
+    R, Z = get_major_radius_coordinates(shot_number)
+
+    if (time_start is None) & (time_end is None):
+        time_array, _ = get_apd_frames(shot_number)
+        time_start, time_end = time_array.min(), time_array.max()
+
+    time = np.linspace(time_start, time_end, resolution)
+
+    apd_pixel_list = np.zeros((90, 2))
+    for i in range(90):
+        apd_pixel_list[i] = (i % 10, int(i / 10))
+
+    apd_pixel_list = apd_pixel_list.astype(int).tolist()
+    pixel_array_length = len(apd_pixel_list)
+
+    # Make a list of the R, Z coordinates
+    major_radius_position = []
+
+    for i in range(pixel_array_length):
+
+        R_position = R[apd_pixel_list[i][0], apd_pixel_list[i][1]]
+        Z_position = Z[apd_pixel_list[i][0], apd_pixel_list[i][1]]
+
+        RZ_pair = [R_position, Z_position]
+        major_radius_position.append(RZ_pair)
+
+    major_radius_R = np.array(major_radius_position)[:, 0]
+    major_radius_Z = np.array(major_radius_position)[:, 1]
+
+    rho_array = efit_major_radius_to_rho(
+        R=major_radius_R,
+        Z=major_radius_Z,
+        time_array=time,
+        shot_number=shot_number,
+        tree=tree,
+    )
+
+    rho_mean = np.mean(rho_array, axis=1)
+    rho_time_averaged = np.swapaxes(np.reshape(rho_mean, (9, 10)), 0, 1)
+
+    return R, Z, rho_time_averaged
